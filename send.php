dc15683e8989eaaa164ace1923af9b81be187c86
lukmention <34057866+lukmention@users.noreply.github.com>
2018-01-19 15:34:06 +0700

    bersih2
    hehehehe

--- send.php
+++
@@ -1,7 +0,0 @@
-<?php
-$noTujuan = "+6281333104139";
-$message = "loha";
- 
-exec('c:\xampp\htdocs\sikol\bin\gammu-smsd-inject.exe -c c:\xampp\htdocs\smsku\bin\smsdrc EMS '.$noTujuan.' -text "'.$message.'"');
- 
-?>
