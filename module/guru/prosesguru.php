b5cf62fab27266abc6fb5c8b4d69e73dc8ec8b43
lukmention <34057866+lukmention@users.noreply.github.com>
2018-01-26 08:51:01 +0700

    header link udah betul

--- module/guru/prosesguru.php
+++ module/guru/prosesguru.php
@@ -7,11 +7,11 @@ if($aksi == "tambah"){
 	
 	$pass=md5($_POST['pass_guru']);
 	$db->input($_POST['nip'],$_POST['nama_guru'],$_POST['jk'],$pass,$_POST['alamat_guru'],$_POST['telp_guru']);
-	header("location:tampilguru.php");
+	header("location:http://localhost/sekolahMadani/admin.php?module=guru&kls=semua");
 }
 elseif($aksi == "hapus"){
 	$db->hapus($_GET['nip']);
-	header("location:tampilguru.php");
+	header("location:http://localhost/sekolahMadani/admin.php?module=guru&kls=semua");
 }
 elseif($aksi == "update"){
 	$db->update($_POST['nip'],$_POST['nama_guru'],$_POST['pass_guru'],$_POST['alamat_guru'],$_POST['telp_guru']);
