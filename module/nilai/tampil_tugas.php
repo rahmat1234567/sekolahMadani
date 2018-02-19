bdb31bfeeca3383518565f20d1ea52af07a4a6bb
Rahmat wibowo <rahmatwibowo66@gmail.com>
2018-02-19 23:30:04 +0700

    nampilkan edit dan hapus tugas dan uh

--- module/nilai/tampil_tugas.php
+++ module/nilai/tampil_tugas.php
@@ -18,11 +18,16 @@
                                     <thead>
                                         <tr>
                                             <th class="text-center">NO</th>
+											<th class="text-center">Nama Siswa</th>
                                             <th class="text-center">Nama Mata Pelajaran</th>
                                             <th class="text-center">Kelas</th>
-                                            <th class="text-center" width="10%">Aksi</th>
+											<th class="text-center">Nama Tugas</th>
+											<th class="text-center">Nilai</th>
+											<th class="text-center">Keterangan</th>
+											<th class="text-center" width="10%">Aksi</th>
                                         </tr>
                                     </thead>
+									
                                     <tbody>
                                                                         
 <?php
@@ -35,19 +40,16 @@ $db = new database();
 	?>	
                                         <tr class="odd gradeX">
                                             <td><?php echo $no++; ?></td>
+											<td><?php echo"$rs[nisn]";  ?></td>
 											<td><?php echo"$rs[id_jadwal]";  ?></td>
                                             <td><?php echo"$rs[id_kls]";  ?></td>
-										
-										<td class="text-center">
-                                            <?php if($level==2){ ?>
-										<a href="./././admin.php?module=input_siswa&act=edit&nisn=<?php echo $rs['nisn'] ?>&level=<?php echo $level; ?>"><button type="button" class="btn btn-info">Edit</button></a> 
-									       <a data-toggle="modal" data-target="#modalHapus<?php echo $noxxx;?>" class="btn btn-danger" >Hapus</a>
-                                        <?php } ?>
-										<a href="./././admin.php?module=detail_siswa&act=details&nisn=<?php echo $rs['nisn'] ?>">
-										<button type="button" class="btn btn-warning" 
-                                        >Details</button> </a>
-										
-										</td>
+											<td><?php echo"$rs[nama_tugas]";  ?></td>
+											<td><?php echo"$rs[nilai_tugas]";  ?></td>
+											<td><?php echo"$rs[ket]";  ?></td>
+											<td class="text-center">
+												<a href="./././admin.php?module=input_nilaitugas&act=edit=<?php echo $rs['nisn'] ?>&level=<?php echo $level; ?>"><button type="button" class="btn btn-info">Edit</button></a> 
+												   <a data-toggle="modal" data-target="#modalHapus<?php echo $noxxx;?>" class="btn btn-danger" >Hapus</a>
+											</td>
                                         </tr>
 	<?php
 }
