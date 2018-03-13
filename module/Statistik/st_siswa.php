b68923169867d9c327dddae3f1ef534a2f7fbdd8
Muflihfikri <35672876+Muflihfikri@users.noreply.github.com>
2018-03-13 10:40:24 +0700

    wkwkwkw

--- module/Statistik/st_siswa.php
+++ module/Statistik/st_siswa.php
@@ -1,3 +1,4 @@
+
 <head>
         <title>Belajarphp.net - ChartJS</title>
         
@@ -32,11 +33,12 @@
                 type: 'line',
                 data: {
                     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+
                     datasets: [{
                             label: '# of Votes',
                             data: [12, 19, 3, 5, 2, 3],
                             backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
+                                  'rgba(255, 99, 132, 0.2)',
                                 'rgba(54, 162, 235, 0.2)',
                                 'rgba(255, 206, 86, 0.2)',
                                 'rgba(75, 192, 192, 0.2)',
